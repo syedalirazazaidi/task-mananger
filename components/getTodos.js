87ffc5782709057d10b01805fd86d6f07d00c68c
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-10-15 04:11:53 +0500

    add delete func

--- components/getTodos.js
+++ components/getTodos.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { collection, addDoc, getDocs, deleteDoc } from 'firebase/firestore'
+import { collection, addDoc, getDocs, doc, deleteDoc } from 'firebase/firestore'
 import { db } from '../firebase'
 import { DeleteIcon, EditIcon } from '@chakra-ui/icons'
 import {
@@ -11,63 +11,88 @@ import {
   Stack,
   Container,
   HStack,
+  Button,
 } from '@chakra-ui/react'
 
 const GetAllTodos = ({ setTextArray, textArray }) => {
-  const deleteTodo = (id) => {
-    console.log(id, 'ID')
-    // const collectionById = collection(db, 'todos', id)
-    // console.log(collectionById)
+  // useEffect(() => {
+  //   db.collection('todos')
+  //     .orderBy('createdAt', 'desc')
+  //     .onSnapshot((snapshot) => {
+  //       setTextArray(snapshot.docs.map((doc) => console.log(doc)))
+  //     })
+  // }, [])
+  useEffect(() => {
+    getNotes()
+  }, [textArray])
 
-    // deleteDoc(collectionById).then(() => {
-    //   window.location.reload()
-    // })
-  }
   const getNotes = async () => {
     const gettodos = collection(db, 'todos')
     const todoSnapshot = await getDocs(gettodos)
-    const todoList = todoSnapshot.docs.map((doc) => doc.data())
-    console.log(todoList)
-    return setTextArray(todoList)
+
+    setTextArray(
+      todoSnapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          text: doc.data().text,
+          createdAt: doc.data().createdAt,
+        }
+      }),
+    )
   }
-  useEffect(() => {
-    getNotes()
-  }, [])
+
+  //DELETE FUNCTION
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, 'todos', id))
+  }
+
+  // EDIT FUNCTION
+
+  const handleEdit = async (id) => {}
 
   if (!textArray.length) {
     return <p>Loading...</p>
   }
+
   return (
     <Box>
-      {textArray.map((text) => (
-        <HStack spacing={140}>
-          <Text w="270px" my={3} mx={10} textAlign="left" ml="40px">
-            {text.text}
-          </Text>
-          <Box>
-            <Tooltip label="delete" placement="bottom">
-              <DeleteIcon
-                _hover={{ color: 'red.300', cursor: 'pointer' }}
-                marginLeft="20px"
-                w={6}
-                h={6}
-                color="grey"
-                mx="10px"
-                onClick={deleteTodo(text._Id)}
-              />
-            </Tooltip>
-            <Tooltip label="Edit" placement="bottom">
-              <EditIcon
-                _hover={{ color: 'green.400', cursor: 'pointer' }}
-                w={6}
-                h={6}
-                color="grey"
-                mx="10px"
-              />
-            </Tooltip>
-          </Box>
-        </HStack>
-      ))}
+      {textArray &&
+        textArray.map((text) => (
+          <HStack spacing={40} key={text.id}>
+            <Text
+              w="270px"
+              my={3}
+              mx={['36px', '10px', '32px']}
+              textAlign={['center', 'left', 'left']}
+              ml={['92px', '40px', '40px']}
+            >
+              {text.text}
+            </Text>
+            <Box display="flex" mt="4px" alignItems="center">
+              <Tooltip label="delete" placement="bottom">
+                <DeleteIcon
+                  _hover={{ color: 'red.300', cursor: 'pointer' }}
+                  marginLeft="20px"
+                  w={6}
+                  h={6}
+                  color="grey"
+                  mx="10px"
+                  onClick={() => handleDelete(text.id)}
+                />
+              </Tooltip>
+              <Tooltip label="Edit" placement="bottom">
+                <EditIcon
+                  _hover={{ color: 'green.400', cursor: 'pointer' }}
+                  w={6}
+                  h={6}
+                  color="grey"
+                  mx="10px"
+                  onClick={() => handleEdit(text.id)}
+                />
+              </Tooltip>
+            </Box>
+          </HStack>
+        ))}
     </Box>
   )
 }
